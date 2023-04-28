f96568a961b3b602a7fbd8e8c83b32f5eb969886
ramiv212 <ramiv212@hotmail.com>
2023-04-28 08:40:31 -0500

    testing websockets online

--- remote/routes.js
+++ remote/routes.js
@@ -7,6 +7,7 @@ import ActiveRemoteSession from "./ActiveRemoteSession.js"
 import { activeSessions } from "./activesessions.js"
 import path from "path"
 
+const PORT = process.env.PORT ?? 5000;
 
 const app = express();
 const httpServer = createServer(app);
@@ -38,20 +39,21 @@ export function doesThisRoomExist(activeSessions,roomId) {
 
 app.post("/create-session", (req,res) => {
     const roomId = req.body.roomId;
-    console.log("**** Ative sessions:")
-    console.log(activeSessions)
+    console.log("**** Active sessions:")
     const currentSession = doesThisRoomExist(activeSessions,roomId);
     
     if (currentSession) {
         console.log(currentSession)
         console.log(`Added remote client to session of ID: ${currentSession.uuid}`);
-        res.send({sessionId: currentSession.uuid})
+        res.send({
+            sessionId: currentSession.uuid});
     } 
     
     else {
         const newSession = new ActiveRemoteSession(roomId);
         console.log(`Created new session of ID: ${newSession.uuid}`);
-        res.send({sessionId: newSession.uuid})
+        res.send({
+            sessionId: newSession.uuid});
     };
 });
 
@@ -62,7 +64,9 @@ app.post("/get-session-id", (req,res) => {
     const currentSession = doesThisRoomExist(activeSessions,roomId);
     
     if (currentSession) {
-        console.log(currentSession)
+        console.log(`Remote User: ${currentSession.localUser}`);
+        console.log(`Remote User: ${currentSession.remoteUser}`);
+
         console.log(`Added local client to session of ID: ${currentSession.uuid}`);
         res.send({sessionId: currentSession.uuid})
     } 
@@ -83,7 +87,6 @@ app.get("/session",(req,res) => {
 
 
 export default function startServer() {
-    const PORT = process.env.PORT ?? 5000;
     httpServer.listen(PORT,() => {
         console.log(`Listening on port: ${PORT}`)
     });
